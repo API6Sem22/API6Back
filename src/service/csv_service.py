78e2846465b2edd5636f9b98f7e0aa28da943964
Gabriela Momilli <gabsmomilli@gmail.com>
2022-10-05 21:33:52 -0300

    transform values in double

--- src/service/csv_service.py
+++ src/service/csv_service.py
@@ -13,10 +13,18 @@ class Csv_service:
     @staticmethod
     def transform_fields(data):
         columns = pd.DataFrame(data)
-        transform_columns = []
+        transform_columns_data = []
+        transform_columns_value = []
         for col in columns:
             if 'dt_' in col:
-                transform_columns.append(col)
-        for item in transform_columns:
+                transform_columns_data.append(col)
+        for item in transform_columns_data:
             data[item] = pd.to_datetime(data[item], format='%Y/%m/%d')
+
+        for col in columns:
+            if 'valor' in col: 
+                transform_columns_value.append(col)
+        for item in transform_columns_value:
+            data[item] = data[item].apply(pd.to_double)
+            #data[item] = data[item].astype(double)
         return data
