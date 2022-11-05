da575b86964b81717c26569f354afec74be82f78
HPereiraVitor <vitor.pereira11@fatec.sp.gov.br>
2022-11-04 21:07:19 -0300

    Implementando inserção criptografada

---
+++ src/service/crypto/crypto.py
@@ -0,0 +1,36 @@
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+import os
+import pyodbc
+
+class crypto:
+
+    @staticmethod
+    def encrypt(marca_otica, cli_nome, cod_num):
+        load_dotenv()
+        server = os.environ['DW_SERVER']
+        user = os.environ['DW_USER']
+        pwd = os.environ['DW_PASS']
+        db = "denddev"
+        driver = "SQL Server"
+
+        dados_conexao = "Driver={"+driver+"}; Server="+server+"; Database="+db+"; ENCRYPT=yes; UID="+user+"; PWD="+pwd+";"
+        conexao = pyodbc.connect(dados_conexao)
+
+        cursor = conexao.cursor()
+
+        key = Fernet.generate_key()
+        cipher_suite = Fernet(key)
+
+        enc_nome     =  cipher_suite.encrypt(bytes(cli_nome, 'utf-8'))
+        enc_contrato = cipher_suite.encrypt(bytes(str(cod_num), 'utf-8'))
+
+        comando = f"""INSERT INTO KEY_CRYPTO(kcr_key, kcr_id)
+                        VALUES ('{key.decode('utf-8')}', '{marca_otica}')"""
+
+        cursor.execute(comando)
+        cursor.commit()
+
+        conexao.close()
+
+        return enc_nome, enc_contrato
