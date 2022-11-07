ce6850eab8509b75d6ee3ced655dfbc3364a2b0c
Gabriela Momilli <55815856+gabsmomilli@users.noreply.github.com>
2022-11-06 23:01:03 -0300

    Feature/logging (#6)
    * adicionando logs a aplicação
    
    * Log Script carga DW
    
    * persistindo logs no banco
    
    * persistindo logs no banco
    
    * adc logs
    
    Co-authored-by: moonees <ramonzxz@icloud.com>
    Co-authored-by: Gabriel Soares Gonçalves <gabrielsoaresg20@gmail.com>

--- src/service/db_service.py
+++ src/service/db_service.py
@@ -1,21 +1,23 @@
+import logging
 from src.configs.database import Database_configs
 from src.service.csv_service import Csv_service
+from src.service.logs.loggers import LoggerConfiguration
 
 
 class Db_service:
 
+    global logger
+    logger = logging.getLogger()
+
     @staticmethod
-    def insert_db():
+    def insert_into_db():
         post = Csv_service.read_csv()
-        collection_name = Database_configs.get_collection_db()
-        collection_name.insert_many(post)
-
-    @staticmethod
-    def get_data_db():
-        collection_name = Database_configs.get_collection_db()
-        results = collection_name.find()
-        for result in results:
-            print(result)
-
-
-Db_service.insert_db()
+        collection_name = Database_configs.get_collection_db(True)
+        try:
+            collection_name.insert_many(post)
+            logger.info('Insert in database with success!')
+        except Exception as e:
+            logger.error('Insert error: ' + str(e))
+        finally:
+            logger.info('Persisti logs in database')
+            LoggerConfiguration.save_logs()
